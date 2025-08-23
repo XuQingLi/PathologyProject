2b3845af1dd7def00c9c1e8ec1ba8143eee0bda5
XuQingLi <2542398121@qq.com>
2025-08-24 00:43:42 +0800

    bash

--- extract_feature_lxq.sh
+++ extract_feature_lxq.sh
@@ -1,17 +1,12 @@
 export CUDA_VISIBLE_DEVICES=0,2,3
 
-# 执行特征提取脚本
-#!/bin/bash
-# 运行特征提取脚本
-
-#!/bin/bash
 python ../extract_feature_lxq.py \
   --slide_dir /mnt/gemlab_data_2/User_database/zhushiwei/PHASE/train_images \
   --out_dir /mnt/gemlab_data_3/LXQ/PHASE/featuresZarrLatest \
   --device cuda \
   --patch 224 \
   --stride 224 \
-  --batch_size 64 \
+  --batch_size 256 \
   --tissue_thr 0.05 \
   --gpus 0,1,2 \
   --num_workers 3 
